6d16be7a3915441864e6388074add027cf5084b1
Maksym <maksym.kozachuk@protonmail.com>
2020-03-02 21:21:00 +0100

    UserDao

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -14,25 +14,25 @@ public class UserDao {
 
     public void save (User user){
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
         session.save(user);
-        tx1.commit();
+        transaction.commit();
         session.close();
     }
 
     public void update(User user){
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
         session.update(user);
-        tx1.commit();
+        transaction.commit();
         session.close();
     }
 
     public void delete(User user){
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
         session.delete(user);
-        tx1.commit();
+        transaction.commit();
         session.close();
     }
 
